3562dca0e8a19a889d8bb307b64317c03b6da20d
Sstark97 <aitorscinfo@gmail.com>
2023-03-27 17:33:47 +0100

    Add a dummy test of register

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,11 @@ const customJestConfig = {
     setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
     moduleDirectories: ["node_modules", "<rootDir>/"],
     testEnvironment: "jest-environment-jsdom",
+    moduleNameMapper: {
+        "@components/(.*)": "<rootDir>/src/components/$1",
+        "@containers/(.*)": "<rootDir>/src/containers/$1",
+        "@pages/(.*)": "<rootDir>/src/pages/$1",
+    },
 }
 
 module.exports = createJestConfig(customJestConfig)
