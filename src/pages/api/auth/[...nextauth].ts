71aeb783358d7c576451df51d46bfac2408c1c70
Sstark97 <aitorscinfo@gmail.com>
2023-03-28 20:24:25 +0100

    Add Github and Google Autentication

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,26 @@
 import NextAuth from "next-auth"
 import type { NextAuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
+import Github from "next-auth/providers/github"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import Google from "next-auth/providers/google"
+
+const prisma = new PrismaClient()
 
 const options: NextAuthOptions = {
     theme: {
         colorScheme: "light",
     },
     debug: true,
-    session: {},
-    jwt: {},
+    adapter: PrismaAdapter(prisma),
     providers: [
-        CredentialsProvider({
-            type: "credentials",
-            name: "ChatNow",
-            async authorize(credentials, req) {},
+        Github({
+            clientId: process.env.AUTH_GITHUB_ID as string,
+            clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+        }),
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID as string,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
         }),
     ],
 }
