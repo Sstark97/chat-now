1fa3cec0d33d257a21d3429f8412e2bf87d35846
Sstark97 <aitorscinfo@gmail.com>
2023-03-29 12:02:32 +0100

    Test(red): call_an_event_when_register_clicked

--- src/test/register.test.tsx
+++ src/test/register.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react"
 import AuthHeader from "@components/AuthHeader"
 import RegisterForm from "@containers/RegisterForm"
+import registerForm from "@containers/RegisterForm"
 
 describe("Register", () => {
     it("renders a heading", () => {
@@ -11,5 +12,17 @@ describe("Register", () => {
 
         expect(heading).toBeInTheDocument()
         expect(container).toMatchSnapshot()
+    })
+
+    it("call an event when register clicked", () => {
+        const register = jest.fn()
+        const { container } = render(<RegisterForm onRegister={register} />)
+        const registerBtn = container.querySelector("button")
+
+        if (registerBtn) {
+            registerBtn.click()
+        }
+
+        expect(register).toHaveBeenCalled()
     })
 })
