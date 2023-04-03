c02ac5bdbf3236388ba16c2bc557e63cc101a168
Sstark97 <aitorscinfo@gmail.com>
2023-04-03 11:56:07 +0100

    Add comments to UserService

--- src/lib/services/UserService.ts
+++ src/lib/services/UserService.ts
@@ -1,9 +1,23 @@
 import { Credentials, UserRepository, UserResponse } from "@customTypes/domain"
 
+/**
+ * @class UserService
+ * @description Servicio para manejar la lógica de negocio de los usuarios
+ * @example
+ * const userService = new UserService(userRepository)
+ */
 class UserService {
     // eslint-disable-next-line no-unused-vars
     constructor(private readonly userRepository: UserRepository) {}
 
+    /**
+     * @method register
+     * @description Registra un nuevo usuario
+     * @param credentials
+     * @returns {Promise<UserResponse | null>}
+     * @example
+     * const userRegistered = await userService.register(user)
+     */
     async register(credentials: Credentials): Promise<UserResponse | null> {
         if (await this.existUserFrom(credentials)) {
             return null
@@ -24,6 +38,15 @@ class UserService {
         }
     }
 
+    /**
+     * @private
+     * @method existUserFrom
+     * @description Verifica si un usuario existe
+     * @param credentials
+     * @returns {Promise<boolean>}
+     * @example
+     * const userExists = await userService.existUserFrom(credentials)
+     */
     private async existUserFrom(credentials: Credentials) {
         const email = credentials?.email as string
 
