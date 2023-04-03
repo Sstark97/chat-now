d9b931717d2bdd0cd35edccb3283037588b56c8c
Sstark97 <aitorscinfo@gmail.com>
2023-04-03 12:01:51 +0100

    Add comments to fetcher

--- src/lib/utils/fetcher.ts
+++ src/lib/utils/fetcher.ts
@@ -1,3 +1,11 @@
+/**
+ * @description Función para hacer una petición POST a un endpoint
+ * @param credentials
+ * @param endPoint
+ * @returns {Promise<any>}
+ * @example
+ * const response = await postFrom(credentials, endPoint)
+ */
 async function postFrom<C>(credentials: Record<keyof C, string> | undefined, endPoint: string) {
     const res = await fetch(endPoint, {
         method: "POST",
