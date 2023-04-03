618429ce6f7122f45756cbf1f8ed51a685b6c463
Sstark97 <aitorscinfo@gmail.com>
2023-04-03 12:00:46 +0100

    Add comments to UserFactory

--- src/lib/factories/UserFactory.ts
+++ src/lib/factories/UserFactory.ts
@@ -1,7 +1,19 @@
 import { UserPrismaRepository } from "@lib/repositories/UserPrismaRepository"
 import { UserService } from "@lib/services/UserService"
 
+/**
+ * @class UserFactory
+ * @description Factoría para crear instancias de UserService
+ */
 class UserFactory {
+    /**
+     * @static
+     * @method createUserService
+     * @description Crea una instancia de UserService
+     * @returns {UserService}
+     * @example
+     * const userService = UserFactory.createUserService()
+     */
     static createUserService() {
         const userPrismaRepository = new UserPrismaRepository()
         return new UserService(userPrismaRepository)
