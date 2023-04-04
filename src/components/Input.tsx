f7a1c3940c07e4f5542f77e2ce008ea73da26eee
Sstark97 <aitorscinfo@gmail.com>
2023-04-04 17:12:29 +0100

    Resolve border errors classNames in Input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -13,31 +13,22 @@ import { useState, FormEvent } from "react"
  */
 const Input = ({ type, placeholder, name, children, errorManager }: InputProps) => {
     const [error, setError] = useState("")
-    //const [border, setBorder] = useState("")
+    const [border, setBorder] = useState("")
+
+    const inputClass = "bg-secondary text-white placeholder-white border rounded-xl px-3 py-2 mt-5 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
 
     const defineError = (e: FormEvent<HTMLInputElement>) => {
         if (errorManager) {
             const element = e.currentTarget
             setError(errorManager.validate(element.value) ? "" : errorManager.errorMessage)
-            //setBorder(errorManager.validate(element.value) ? "border: 2px; border-color: green" : "border-2 border-busy")
+            setBorder(errorManager.validate(element.value) ? "border-2 border-success" : "border-2 border-busy")
         }
     }
-
-    const inputClass = "bg-secondary text-white placeholder-white border border-gray-300 rounded-xl px-3 py-2 mt-5 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
 
     return (
         <>
             <div className="relative flex flex-col mx-auto">
-                <input
-                    type={type}
-                    placeholder={placeholder}
-                    className={inputClass}
-                    style={{error === "" ? "border: 2px; border-color: green" : "border: 2px; border-color: red"}}
-                    autoComplete="new-password"
-                    name={name}
-                    required
-                    onBlur={defineError}
-                />
+                <input type={type} placeholder={placeholder} className={`${inputClass} ${border}`} autoComplete="new-password" name={name} required onBlur={defineError} />
                 {children}
             </div>
             {error ? <p className="text-busy text-sm mt-1 opacity-60">{error}</p> : null}
