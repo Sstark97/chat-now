42201177a5a7d18169731c7b78e7796facb113c4
Sstark97 <aitorscinfo@gmail.com>
2023-04-08 14:02:05 +0100

    Change eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,7 +8,6 @@ module.exports = {
     extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:jsx-a11y/recommended", "plugin:prettier/recommended", "next", "next/core-web-vitals", "plugin:jest/recommended"],
     parser: "@typescript-eslint/parser",
     plugins: ["@typescript-eslint"],
-    root: true,
     rules: {
         "prettier/prettier": [
             "error",
