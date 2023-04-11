8adb99b3fc4a63546ee03f2f43197b0467f51272
Sstark97 <aitorscinfo@gmail.com>
2023-04-11 13:30:04 +0100

    Test(green): check if the button is enable when all inputs are correct

--- src/test/register.test.tsx
+++ src/test/register.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react"
 import { ChatProvider } from "@context/ChatProvider"
 import AuthHeader from "@components/AuthHeader"
 import RegisterForm from "@containers/RegisterForm"
+import userEvent from "@testing-library/user-event"
 
 jest.mock("next/router", () => require("next-router-mock"))
 
@@ -38,5 +39,30 @@ describe("Register", () => {
         const registerBtn = screen.getByText("Crear cuenta")
 
         expect(registerBtn).toHaveAttribute("disabled")
+    })
+
+    it("check if the button is enable when all inputs are correct", async () => {
+        const user = userEvent.setup()
+
+        render(
+            <ChatProvider>
+                <RegisterForm />
+            </ChatProvider>
+        )
+
+        const registerBtn = screen.getByText("Crear cuenta")
+        const nameInput = await screen.findByPlaceholderText("Nombre")
+        const emailInput = await screen.findByPlaceholderText("Correo electrónico")
+        const passwordInput = await screen.findByPlaceholderText("Contraseña")
+
+        await user.type(nameInput, "irrelevant name")
+        await user.type(emailInput, "irrelevant@email.com")
+        await user.type(passwordInput, "irrelevant_password1234")
+
+        await user.click(nameInput)
+        await user.click(emailInput)
+        await user.click(passwordInput)
+
+        expect(registerBtn).not.toHaveAttribute("disabled")
     })
 })
