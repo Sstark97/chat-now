b39c7b054876ae0b5df06a7ed9d67880367ec7a5
Sstark97 <aitorscinfo@gmail.com>
2023-04-13 12:04:23 +0100

    login.test.tsx Test(green): should enable button when all fields are correct

--- src/test/login.test.tsx
+++ src/test/login.test.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react"
 import { ChatProvider } from "@context/ChatProvider"
 import LoginForm from "@containers/LoginForm"
-import { AUTH_BUTTONS } from "@lib/constants/authForms"
+import { AUTH_BUTTONS, INPUT_LOGIN_PLACEHOLDER } from "@lib/constants/authForms"
+import userEvent from "@testing-library/user-event"
 
 jest.mock("next/router", () => require("next-router-mock"))
 
@@ -17,5 +18,26 @@ describe("Login", () => {
 
         expect(loginBtn).toBeInTheDocument()
         expect(loginBtn).toHaveAttribute("disabled")
+    })
+
+    it("should enable button when all fields are correct", async () => {
+        const user = userEvent.setup()
+
+        render(
+            <ChatProvider>
+                <LoginForm />
+            </ChatProvider>
+        )
+
+        const loginBtn = screen.getByText(AUTH_BUTTONS.LOGIN)
+        const inputEmail = screen.getByPlaceholderText(INPUT_LOGIN_PLACEHOLDER.EMAIL)
+        const inputPassword = screen.getByPlaceholderText(INPUT_LOGIN_PLACEHOLDER.PASSWORD)
+
+        await user.type(inputEmail, "irrelevant@email.com")
+        await user.type(inputPassword, "irrelevantPassword")
+
+        await user.click(document.body)
+
+        expect(loginBtn).not.toHaveAttribute("disabled")
     })
 })
