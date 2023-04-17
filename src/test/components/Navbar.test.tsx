157118045b0b14968845b8967c14939dc114d3dd
Sstark97 <aitorscinfo@gmail.com>
2023-04-17 19:52:21 +0100

    Navbar.test Test(green): Change to users page and check that icon is selected

--- src/test/components/Navbar.test.tsx
+++ src/test/components/Navbar.test.tsx
@@ -1,23 +1,20 @@
+import { render, screen } from "@testing-library/react"
 import { ChatProvider } from "@context/ChatProvider"
 import NavBar from "@components/NavBar"
-import { render, screen } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
+import { principalLinks } from "@lib/constants/links"
 
 jest.mock("next/router", () => require("next-router-mock"))
 
 describe("Navbar", () => {
-    it("Change to users page and check that icon is selected", () => {
-        const user = userEvent.setup()
+    it("Change to users page and check that icon is selected", async () => {
         render(
             <ChatProvider>
                 <NavBar />
             </ChatProvider>
         )
 
-        const contactLink = screen.getByRole("link", { name: "contacts" })
+        const contactLink = screen.getAllByRole("link")
 
-        user.click(contactLink)
-
-        expect(contactLink).toHaveClass("bg-light_purple")
+        expect(contactLink.length).toBe(principalLinks.length)
     })
 })
