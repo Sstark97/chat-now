6a12bf0e3c475f3977603c6c60f119286b6b37d1
SaraCS21 <saracs15.scssn@gmail.com>
2023-04-19 18:12:26 +0100

    Add errors

--- src/pages/api/contacts/index.ts
+++ src/pages/api/contacts/index.ts
@@ -1,43 +1,10 @@
 import { NextApiResponse } from "next"
 import { getSession } from "next-auth/react"
 import { UserFactory } from "@lib/factories/UserFactory"
-import type { User } from "@prisma/client"
-import type { ContactRequest, ValidateResponse } from "@customTypes/request"
-import type { ErrorResponse } from "@customTypes/domain"
+import type { ContactRequest } from "@customTypes/request"
+import type { ErrorResponse, Contacts } from "@customTypes/domain"
 
 const userService = UserFactory.createUserService()
-
-/**
- * Comprueba si hay errores en los datos del usuario
- * @param req {UserRequest}
- * @param res {NextApiResponse<ErrorResponse>}
- * @returns {Promise<void>}
- * @example
- * await checkErrorsInRegisterFrom(req, res)
- */
-// const checkErrorsFrom = async (req: ContactRequest, res: NextApiResponse<ErrorResponse>) => {
-//     const session = await getSession({ req })
-//     const userEmail = session?.user?.email as string
-//     const { email: contactEmail } = req.body
-//     const response = {} as ValidateResponse
-
-//     if (req.method !== "POST") {
-//         res.status(405).end()
-//     }
-
-//     const userAlreadyExists = await userService.existUserFrom(contactEmail)
-//     const isContactAdded = await userService.isTheContactAddedBy(userEmail, contactEmail)
-
-//     if (!userAlreadyExists) {
-//         response.status = 400
-//         response.error = `No existe un usuario con el email ${contactEmail}`
-//     } else if (isContactAdded) {
-//         response.status = 400
-//         response.error = `El usuario con el email ${contactEmail} ya está añadido`
-//     }
-
-//     return response
-// }
 
 /**
  * @description Manejador de la ruta /api/contacts/add
@@ -47,15 +14,18 @@ const userService = UserFactory.createUserService()
  */
 export default async function handler(
     req: ContactRequest,
-    res: NextApiResponse<User[] | ErrorResponse>
+    res: NextApiResponse<Contacts[] | ErrorResponse>
 ) {
-    // const errorResponse = await checkErrorsFrom(req, res)
-
-    // if (errorResponse.status) {
-    //     return res.status(errorResponse.status).json({ message: errorResponse.error })
-    // }
+    if (req.method !== "GET") {
+        res.status(405).end()
+    }
 
     const session = await getSession({ req })
+
+    if (!session) {
+        return res.status(401).end()
+    }
+
     const userEmail = session?.user?.email as string
 
     const contacts = await userService.getContactsFrom(userEmail)
