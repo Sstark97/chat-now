d8cd611c3c04fdcf0150a7ebd2a66a77cde87c40
Sstark97 <aitorscinfo@gmail.com>
2023-05-04 19:21:47 +0100

    Add useEditContact hook

--- src/pages/contacts/modify.tsx
+++ src/pages/contacts/modify.tsx
@@ -1,38 +1,16 @@
 import useChatContext from "@hooks/useChatContext"
+import useEditContact from "@hooks/useEditContact"
+import { FaUserAlt } from "react-icons/fa"
 import NavBar from "@components/NavBar"
 import ChatDesktop from "@components/ChatDesktop"
-import { API, REDIRECT } from "@lib/constants/links"
 import InputWithIcon from "@components/InputWithIcon"
 import Button from "@components/Button"
 import Error from "@components/Error"
-import { FaUserAlt } from "react-icons/fa"
 import { errors } from "@lib/constants/validations"
-import { changeFrom } from "@lib/utils/fetcher"
-import { getUserDataFrom } from "@lib/utils/user"
-import { useState } from "react"
-import { useRouter } from "next/router"
 
 const ModifyContactContainer = () => {
-    const [error, setError] = useState("")
-    const router = useRouter()
-    const { ref, selectedChat, reloadContacts, handleChangeSelectedChatName } = useChatContext()
-
-    const handleEdit = async () => {
-        const contactBody = getUserDataFrom(ref.current)
-        try {
-            await changeFrom(
-                { ...contactBody, id: selectedChat.id as string },
-                API.MODIFY_CONTACT,
-                "PUT"
-            )
-            await reloadContacts()
-            handleChangeSelectedChatName(contactBody.name)
-            await router.push(REDIRECT.CONTACTS)
-        } catch (error) {
-            const { message } = error as Error
-            setError(message)
-        }
-    }
+    const { ref, selectedChat } = useChatContext()
+    const { handleEdit, error } = useEditContact()
 
     const inputClass = "w-[80%] mt-5 mb-1"
     const errorClass = "w-[80%] mb-1"
