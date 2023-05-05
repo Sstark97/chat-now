2259b24ab5a45a266424ae819a81217985960708
SaraCS21 <saracs15.scssn@gmail.com>
2023-05-05 17:45:23 +0100

    Modify style in modify contact page

---
+++ src/components/ContactNavBar.tsx
@@ -0,0 +1,39 @@
+import Link from "next/link"
+import { MdArrowBackIosNew } from "react-icons/md"
+import Image from "next/image"
+import useChatContext from "@hooks/useChatContext"
+
+const ContactNavBar = () => {
+    const { ref, selectedChat } = useChatContext()
+
+    return (
+        <>
+            <nav
+                className="w-full bg-secondary flex justify-evenly lg:items-center lg:justify-between py-8"
+                ref={ref}
+            >
+                <Link className="absolute top-7 left-0" href="/contacts">
+                    <MdArrowBackIosNew className="ml-6 font-extrabold text-xl" />
+                </Link>
+
+                <div className="w-full flex justify-center items-center flex-col">
+                    {selectedChat.image ? (
+                        <Image
+                            className="w-[8rem] h-[8rem] rounded-full"
+                            src={selectedChat.image}
+                            alt="user"
+                            width={75}
+                            height={75}
+                        />
+                    ) : (
+                        <div className="w-[8rem] h-[8rem] lg:bg-secondary bg-icon rounded-full"></div>
+                    )}
+                    <p className="text-center text-2xl mt-5">{selectedChat.name}</p>
+                    <p className="opacity-50 mt-3">En línea</p>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default ContactNavBar
