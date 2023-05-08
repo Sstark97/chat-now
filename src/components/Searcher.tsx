34a1f7f4f905b7bd0db6f5cb8bebb71c91f0a6b4
SaraCS21 <saracs15.scssn@gmail.com>
2023-05-08 21:44:48 +0100

    Fix types searcher

--- src/components/Searcher.tsx
+++ src/components/Searcher.tsx
@@ -1,12 +1,14 @@
+import { ChangeEvent } from "react"
 import { BsSearch } from "react-icons/bs"
+import type { SearcherProps } from "@customTypes/containers"
 
 /**
  * Este componente se encarga de crear el buscador de la página
  * @returns component
  * @example <Searcher />
  */
-const Searcher = ({ searchText, setSearchText }) => {
-    const handleInputChange = (event) => {
+const Searcher = ({ searchText, setSearchText }: SearcherProps) => {
+    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
         const text = event.target.value
         setSearchText(text)
     }
