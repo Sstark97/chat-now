8e85876d00d70c4ef0155e116a38caf66a8b2de1
Sstark97 <aitorscinfo@gmail.com>
2023-05-08 20:30:23 +0100

    Test(🟢): should find an user by email

--- src/test/lib/services/UserService.test.ts
+++ src/test/lib/services/UserService.test.ts
@@ -29,4 +29,33 @@ describe("UserService", () => {
         expect(userExist).toBeTruthy()
         expect(userRepository.findBy).toHaveBeenCalled()
     })
+
+    it("should find an user by id", async () => {
+        const user: User = {
+            id: "irrelevantId",
+            name: "irrelevant",
+            email: "irrelevant@email.com",
+            password: "irrelevant",
+            image: "irrelevant",
+            status: "busy",
+            emailVerified: new Date(),
+        }
+
+        const users = [user]
+        const userRepository: UserRepository = {
+            findBy: jest.fn(),
+            findByID: jest.fn(() => {
+                return Promise.resolve(users.find((user) => user.id === "irrelevantId") ?? null)
+            }),
+            create: jest.fn(),
+            edit: jest.fn(),
+            delete: jest.fn(),
+            changeStatus: jest.fn(),
+        }
+        const userService = new UserService(userRepository)
+        const userExist = await userService.existUserFromID("irrelevantId")
+
+        expect(userExist).toBeTruthy()
+        expect(userRepository.findByID).toHaveBeenCalled()
+    })
 })
