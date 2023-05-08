150f1f94aa6c35390940e9921fcc232c4390d54b
SaraCS21 <saracs15.scssn@gmail.com>
2023-05-08 21:19:03 +0100

    Add searcher logic

--- src/components/Searcher.tsx
+++ src/components/Searcher.tsx
@@ -5,14 +5,23 @@ import { BsSearch } from "react-icons/bs"
  * @returns component
  * @example <Searcher />
  */
-const Searcher = () => (
-    <div className="bg-secondary dark:bg-dark_secondary w-[90%] flex justify-between items-center mt-7 mx-auto p-3 rounded-2xl">
-        <BsSearch />
-        <input
-            className="w-[88%] bg-transparent placeholder:text-black dark:placeholder:text-white focus:outline-0"
-            type="text"
-            placeholder="Buscar"
-        />
-    </div>
-)
+const Searcher = ({ searchText, setSearchText }) => {
+    const handleInputChange = (event) => {
+        const text = event.target.value
+        setSearchText(text)
+    }
+
+    return (
+        <div className="bg-secondary dark:bg-dark_secondary w-[90%] flex justify-between items-center mt-7 mx-auto p-3 rounded-2xl">
+            <BsSearch />
+            <input
+                className="w-[88%] bg-transparent placeholder:text-black dark:placeholder:text-white focus:outline-0"
+                type="text"
+                placeholder="Buscar"
+                value={searchText}
+                onChange={handleInputChange}
+            />
+        </div>
+    )
+}
 export default Searcher
