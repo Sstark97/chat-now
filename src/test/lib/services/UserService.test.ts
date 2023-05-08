d9bef4987092f06a1a931de5bd1a5698b2db59bf
Sstark97 <aitorscinfo@gmail.com>
2023-05-08 20:23:42 +0100

    Test(🔵): should find an user by email

--- src/test/lib/services/UserService.test.ts
+++ src/test/lib/services/UserService.test.ts
@@ -1,20 +1,22 @@
 import { UserService } from "@lib/services/UserService"
 import { UserRepository } from "@customTypes/domain"
+import { User } from "@prisma/client"
 
 describe("UserService", () => {
     it("should find an user by email", async () => {
+        const user: User = {
+            id: "irrelevant",
+            name: "irrelevant",
+            email: "irrelevant@email.com",
+            password: "irrelevant",
+            image: "irrelevant",
+            status: "busy",
+            emailVerified: new Date(),
+        }
         const userRepository: UserRepository = {
-            findBy: jest.fn(() =>
-                Promise.resolve({
-                    id: "irrelevant",
-                    name: "irrelevant",
-                    email: "irrelevant@email.com",
-                    password: "irrelevant",
-                    image: "irrelevant",
-                    status: "busy",
-                    emailVerified: new Date(),
-                })
-            ),
+            findBy: jest.fn(() => {
+                return Promise.resolve(user)
+            }),
             findByID: jest.fn(),
             create: jest.fn(),
             edit: jest.fn(),
