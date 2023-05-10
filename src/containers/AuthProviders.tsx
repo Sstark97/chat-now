77d277a64d49b32d47c8e800ac663ed15c903718
SaraCS21 <saracs15.scssn@gmail.com>
2023-05-10 19:31:46 +0100

    Change colors

--- src/containers/AuthProviders.tsx
+++ src/containers/AuthProviders.tsx
@@ -28,12 +28,14 @@ const AuthProviders = () => {
 
             <div className="w-full text-center">
                 <div className="w-full flex items-center justify-between">
-                    <div className="w-5/12 h-[.08rem] bg-secondary  bg-opacity-50"></div>
-                    <p className="text-secondary">O</p>
-                    <div className="w-5/12 h-[.08rem] bg-secondary bg-opacity-50"></div>
+                    <div className="w-5/12 h-[.08rem] bg-secondary_text dark:bg-dark_secondary_text bg-opacity-50"></div>
+                    <p className="text-secondary_text dark:text-dark_secondary_text">O</p>
+                    <div className="w-5/12 h-[.08rem] bg-secondary_text dark:bg-dark_secondary_text bg-opacity-50"></div>
                 </div>
 
-                <p className="text-secondary">Por favor, inserte sus datos</p>
+                <p className="text-secondary_text dark:text-dark_secondary_text">
+                    Por favor, inserte sus datos
+                </p>
             </div>
         </>
     )
