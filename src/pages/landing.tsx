02eeb1e78120e23bb41be5d52ccd1cb4400d07cf
Sstark97 <aitorscinfo@gmail.com>
2023-05-10 19:03:03 +0100

    Convert landing to SSG

--- src/pages/landing.tsx
+++ src/pages/landing.tsx
@@ -1,13 +1,40 @@
+import Head from "next/head"
 import Image from "next/image"
 import Features from "@containers/Features"
 import ExampleChats from "@components/ExampleChats"
 import Footer from "@containers/Footer"
 import Hero from "@components/Hero"
 import AuthGuardian from "@containers/AuthGuardian"
+import type { GetStaticProps } from "next"
 
-const Landing = () => {
+export const getStaticProps: GetStaticProps = async () => {
+    return {
+        props: {
+            title: "ChatNow",
+            description: "ChatNow es una aplicación de mensajería instantánea",
+            keywords: "chat, mensajería, instantánea, chatnow",
+        },
+    }
+}
+
+const Landing = ({
+    title,
+    description,
+    keywords,
+}: {
+    title: string
+    description: string
+    keywords: string
+}) => {
     return (
         <AuthGuardian>
+            <Head>
+                <title>{title}</title>
+                <meta name="description" content={description} />
+                <meta name="keywords" content={keywords} />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <>
                 <Image
                     src="/logo.png"
